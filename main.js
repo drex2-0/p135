25bfd01edbd29ed5bfb4a81d75f6bad5f48f1f5b
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-06-06 16:26:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,18 @@ function setup()
 {
     canvas=createCanvas(500,350);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(500,350);
+    video.hide();
+}
+function start()
+{
+    objectdetector=ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML="status: Detecting Objects";
+    objectname=document.getElementById("objectname").value;
+}
+
+function draw()
+{
+    image(video,0,0,500,350);
 }
