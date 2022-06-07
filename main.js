9eca0fd06d8a0bbd14a91fca0cae49ecc79c3640
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-06-07 16:54:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+objects=[];
+status="";
+
 function setup()
 {
     canvas=createCanvas(500,350);
@@ -13,7 +16,48 @@ function start()
     objectname=document.getElementById("objectname").value;
 }
 
+function modelloaded()
+{
+    console.log("Model is Loaded");
+    status=true;
+}
+
+function gotresults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
 function draw()
 {
     image(video,0,0,500,350);
+    if(status!="")
+    {
+        objectdetector.detect(video,gotresults);
+        for(i=0;i<objects.length;i++)
+        {
+            document.getElementById("status").innerHTML="status:objects detected";
+            fill("#ff0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x,objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label==objectname)
+            {
+                
+                objectdetector.detect(gotresults);
+                document.getElementById("status").innerHTML=objectname+" found";
+            }
+            else
+            {
+                document.getElementById("status").innerHTML=objectname+" not found";
+            }
+        }
+    }
 }
